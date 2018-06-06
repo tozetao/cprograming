6d3362a3efbb35016f6015f98f978806584f048e
tozetao <tozetao@163.com>
2018-06-06 19:25:48 +0800

    6-6

--- process/remove_zombie.c
+++ process/remove_zombie.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
 #include <sys/wait.h>
-#include <signal.h>
 
 void read_childproc(int sig)
 {
     int status;
     pid_t id = waitpid(-1, &status, WNOHANG);
     
+    printf("read_childproc: %d\n", id);
+
     if(WIFEXITED(status))
     {
         printf("Removed proc id: %d\n", id);
@@ -27,15 +29,29 @@ int main(int argc, char const *argv[])
     act.sa_flags = 0;
 
     //注册
-    sigaction(SIGALRM, &act, 0);
+    sigaction(SIGCHLD, &act, 0);
 
     pid = fork();
     if(pid == 0){
-        puts("Hi, I'm a child process");
+        puts("Hi, I'm a child process1\n");
         sleep(10);
         return 110;
     }else{
-        
+        printf("Child proc id: %d\n", pid);
+
+        pid = fork();
+        if(pid == 0){
+            puts("Hi, I'm a child process2\n");
+            sleep(12);
+            return 15;
+        }else{
+            int i;
+            printf("Child proc id: %d\n", pid);
+            for(i=0; i<5; i++){
+                puts("wait...\n");
+                sleep(5);
+            }
+        }
     }
 
     return 0;
