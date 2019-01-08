60c113b13c21ac19858db6d42e2264a75ff4b592
tozetao <tozetao@163.com>
2019-01-09 00:57:54 +0800

    1-9

--- socket/client.c
+++ socket/client.c
@@ -1,9 +1,6 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
-#include <unistd.h>
-#include <string.h>
+#include "socket_func.c"
 
 int main() {
     int sock = socket(PF_INET, SOCK_STREAM, 0);
@@ -21,39 +18,9 @@ int main() {
     }    
 
     char buffer[1024];
-    //result = write(sock, "fuck", 5);
-    //printf("result: %d\n", result);
-    //if (result == -1) {
-    //    printf("write error");
-    //    exit(-1);
-    //}
-    //
-    //int i;
-    //for(i = 0; i<3; i++) {
-    //    result = write(sock, "cao", 4);
-    //    printf("result: %d\n", result);
-    //
-    //    if (result == -1) {
-    //        printf("write error");
-    //        exit(-1);
-    //    }
-    //}
    
     read(sock, buffer, 100);
     printf("read from server: %s\n", buffer);
-//    while(1) {
-//        scanf("%s", buffer);
-//        printf("receive: %s\n", buffer);
-//
-//        result = write(sock, buffer, sizeof(buffer));
-//        if (result == -1) {
-//            printf("write error");
-//            exit(-1);
-//        }
-//        memset(buffer, 0, 1024);
-//        // read(sock, buffer, 100);
-//        // printf("read from server: %s\n", buffer);
-//    }
 
     close(sock);
 
--- socket/socket_func.c
+++ socket/socket_func.c
@@ -3,14 +3,29 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <errno.h>
+#include <error.h>
+#include <unistd.h>
 
 //将网络字节序转换成IPv4地址
-char *sock_ntop(const struct sockaddr *sa, socklen_t salen);
 
-int main()
-{
-    return 0;
-}
+//从socket中读取一定字节数据
+
+//int main()
+//{
+//    char *ip;
+//    struct sockaddr_in server_addr;
+//    memset(&server_addr, '0', sizeof(server_addr));
+//
+//    server_addr.sin_port = htons(8080);
+//    server_addr.sin_family = AF_INET;
+//    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+//    
+//    ip = sock_ntop((struct sockaddr*)&server_addr, sizeof(server_addr));
+//    printf("%s\n", ip);
+
+//    return 0;
+//}
 
 char *sock_ntop(const struct sockaddr *sa, socklen_t salen)
 {
@@ -27,9 +42,34 @@ char *sock_ntop(const struct sockaddr *sa, socklen_t salen)
             snprintf(post_str, sizeof(post_str), ":%d", ntohs(sin->sin_port));
             strcat(str, post_str);
         }
+        printf("str: %s; post_str: %s\n", str, post_str);
 
         return str;
     }
     
     return NULL;
+}
+
+ssize_t readn(int fd, void *vpstr, size_t n)
+{
+    size_t left = n;
+    ssize_t readSize;
+    char *buffer;
+
+    buffer = (char *)vpstr;
+
+    while (left > 0) {
+        if ((readSize = read(fd, buffer, left)) < 0) {
+            if (errno == EINTR) 
+                readSize = 0;
+            else
+                return -1;
+        } else if (readSize == 0){
+            return 0;
+        }
+        left = left - readSize;
+        buffer = buffer + readSize;
+    }
+
+    return n - left;
 }
